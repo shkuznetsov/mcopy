e4f801cd7a1baa2a0a8c29004e6748d5401a053a
Alexander Kuznetsov <sasha@monterosa.co.uk>
2018-10-15 22:59:11 +0100

    Improve and jestify the test suite

--- test/parseArguments.test.js
+++ test/parseArguments.test.js
@@ -10,49 +10,64 @@ const assertCopyJob = (job, src, dest) => {
 	expect(job.dest).toBe(dest);
 };
 
-describe('parseArguments', function() {
+test("should support single-job shorthand syntax", () => {
+	let {jobs, opt, callback} = run('src', 'dest', {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src'], 'dest');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support single-job shorthand syntax", function() {
-		let {jobs, opt, callback} = run('src', 'dest', {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src'], 'dest');
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support single-job verbose syntax", () => {
+	let {jobs, opt, callback} = run({src: 'src', dest: 'dest'}, {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src'], 'dest');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support single-job verbose syntax", function() {
-		let {jobs, opt, callback} = run({src: 'src', dest: 'dest'}, {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src'], 'dest');
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support multi-job shorthand syntax", () => {
+	let {jobs, opt, callback} = run([['src1', 'dest1'], ['src2', 'dest2']], {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src1'], 'dest1');
+	assertCopyJob(jobs[1], ['src2'], 'dest2');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support multi-job shorthand syntax", function() {
-		let {jobs, opt, callback} = run([['src1', 'dest1'], ['src2', 'dest2']], {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src1'], 'dest1');
-		assertCopyJob(jobs[1], ['src2'], 'dest2');
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support multi-job verbose syntax", () => {
+	let {jobs, opt, callback} = run([{src: 'src1', dest: 'dest1'}, {src: 'src2', dest: 'dest2'}], {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src1'], 'dest1');
+	assertCopyJob(jobs[1], ['src2'], 'dest2');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support multi-job verbose syntax", function() {
-		let {jobs, opt, callback} = run([{src: 'src1', dest: 'dest1'}, {src: 'src2', dest: 'dest2'}], {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src1'], 'dest1');
-		assertCopyJob(jobs[1], ['src2'], 'dest2');
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support array as a source in a single-job", () => {
+	let {jobs, opt, callback} = run(['src1', 'src2'], 'dest', {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src1', 'src2'], 'dest');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support array as a source", function() {
-		let {jobs, opt, callback} = run(['src1', 'src2'], 'dest', {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src1', 'src2'], 'dest');
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support array as a source in a multi-job", () => {
+	let {jobs, opt, callback} = run([[['src11', 'src12'], 'dest1'], [['src21', 'src22'], 'dest2']], {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src11', 'src12'], 'dest1');
+	assertCopyJob(jobs[1], ['src21', 'src22'], 'dest2');
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
 
-	it("should support function as a destination", function() {
-		let {jobs, opt, callback} = run('src', func, {parallel: 3}, func);
-		assertCopyJob(jobs[0], ['src'], func);
-		expect(opt.parallel).toEqual(3);
-		expect(callback).toBe(func);
-	});
+test("should support function as a destination", () => {
+	let {jobs, opt, callback} = run('src', func, {parallel: 3}, func);
+	assertCopyJob(jobs[0], ['src'], func);
+	expect(opt.parallel).toEqual(3);
+	expect(callback).toBe(func);
+});
+
+test("should support callback without options in a single-job", () => {
+	let {jobs, opt, callback} = run('src', 'dest', func);
+	expect(callback).toBe(func);
+});
+
+test("should support callback without options in a multi-job", () => {
+	let {jobs, opt, callback} = run([['src', 'dest'], ['src', 'dest']], func);
+	expect(callback).toBe(func);
 });
