1bb805b537f5ef39c6d305d77068ae6a18dcf1fa
Alexander Kuznetsov <sasha@monterosa.co.uk>
2018-11-03 23:11:57 +0000

    Unit tests for options defaulting

---
+++ test/defaultOptions.test.js
@@ -0,0 +1,44 @@
+const defaultOptions = require('../lib/defaultOptions');
+
+test("should use supplied value", () => {
+	let opt = defaultOptions({
+		createDir: false,
+		deleteSource: true,
+		parallel: 3,
+		highWaterMark: 1024,
+		globOpt: {silent: false, opt: 1}
+	});
+	expect(opt.createDir).toEqual(false);
+	expect(opt.deleteSource).toEqual(true);
+	expect(opt.parallel).toEqual(3);
+	expect(opt.highWaterMark).toEqual(1024);
+	expect(opt.globOpt.silent).toEqual(false);
+	expect(opt.globOpt.opt).toEqual(1);
+});
+
+test("should default if parameter is missing", () => {
+	let opt = defaultOptions({});
+	expect(opt.createDir).toEqual(true);
+	expect(opt.deleteSource).toEqual(false);
+	expect(opt.parallel).toEqual(1);
+	expect(opt.globOpt.silent).toEqual(true);
+});
+
+test("should deep-default objects", () => {
+	let opt = defaultOptions({
+		globOpt: {opt: 1}
+	});
+	expect(opt.globOpt.silent).toEqual(true);
+	expect(opt.globOpt.opt).toEqual(1);
+});
+
+test("should default if integer parameter is wrong value", () => {
+	let opt = defaultOptions({
+		parallel: -1,
+		highWaterMark: 0,
+		globOpt: () => {}
+	});
+	expect(opt.parallel).toEqual(1);
+	expect(opt.highWaterMark).toEqual(4194304);
+	expect(opt.globOpt).toEqual({silent: true});
+});
