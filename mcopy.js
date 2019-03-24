53b879963b42f67b0f77493304d81deb8f812c1f
Alexander Kuznetsov <sasha@monterosa.co.uk>
2019-03-24 23:05:34 +0000

    Replace 'imports' with 'requires', too early for tomorrow!

--- mcopy.js
+++ mcopy.js
@@ -1,13 +1,12 @@
 'use strict';
 
-import {defaultOptions} from './lib/defaultOptions';
-import {jobsFactory} from './lib/JobsAndCollections';
+const defaultOptions = require('./lib/defaultOptions');
+const InputCollection = require('./lib/InputCollection');
 
 const
 	ProgressEmitter = require('./lib/ProgressEmitter.js');
 
-module.exports = function () {
-
+module.exports = function (...args) {
 	// Inputs parsing
 	let jobs, opt, callback;
 	// Was this a multi-job call?
@@ -15,13 +14,14 @@ module.exports = function () {
 	else jobs = [[args.shift(), args.shift()]];
 	// Is the next argument - opt?
 	if (typeof args[0] === 'object') opt = defaultOptions(args.shift());
+	else opt = defaultOptions();
 	// Is the next argument - callback?
 	if (typeof args[0] === 'function') callback = args[0];
 
 	// This will be returned
 	let emitter = new ProgressEmitter();
 
-	let jobsCollection = new JobsCollection(jobs, opt, emitter);
+	let jobsCollection = new InputCollection(jobs, opt, emitter);
 
 	let promise = jobsCollection.run();
 
@@ -32,73 +32,3 @@ module.exports = function () {
 	// Return the chainable emitter
 	return emitter;
 };
-
-
-
-
-
-function jobsToFiles (jobs, opt, machinegun) {
-	let files = [];
-	// Load up the machinegun with Jobs-to-Files conversion tasks
-	jobs.forEach((job) => machinegun.load(async () => files.push(...await job.toFiles(opt))));
-	// Fire the machinegun and return the promise resolving to the files list
-	return machinegun.fire().promise(files);
-}
-
-function dedupFiles (files) {
-	let bySrc = {}, byDest = {};
-	files.forEach((file) => {
-		// Is it a source duplicate?
-		if (bySrc[file.src]) {
-			// Return if this is a complete duplicate of an existing file
-			for(let i in bySrc[file.src]) if (bySrc[file.src][i].dest == file.dest) return;
-			// ... or if not, add it in
-			bySrc[file.src].push(file);
-		} else {
-			bySrc[file.src] = [file];
-		}
-		// Is it a destination duplicate?
-		if (byDest[file.dest]) {
-			// TODO: Fail with destination duplicate error
-		} else {
-			byDest[file.dest] = true;
-		}
-	});
-	return bySrc;
-}
-
-	const copyFiles = (inputFiles) => {
-		inputFiles.forEach((file) => mg.load(() => {
-			let promise = file.prepareDestination().then((file) => file.copy());
-			// Add deletion to the promise chain only if opt.deleteSource is truthy
-			if (opt.deleteSource) promise = promise.then((file) => file.deleteSource());
-			// Abort copying if the whole machinegun gives up
-			mg.on('giveUp', () => file.abort());
-			return promise;
-		}));
-		return mg.fire().promise();
-	};
-
-	const reportComplete = (err) => {
-		if (err && emitter.listenerCount('error')) emitter.emit('error', err);
-		if (!errored && !err) emitter.emit('success');
-		emitter.emit('complete');
-		if (typeof opt.callback == 'function') opt.callback(err);
-	};
-
-
-
-	return emitter;
-};
-
-/*
-Parse inputs >>
-	CopyJob_PhaseZero: [array of globs] => string|function
-Resolve globs >>
-	CopyJob_PhaseOne: string+stat => string|function
-Resolve destinations >>
-	CopyJob_PhaseTwo: string+stat => string
-Deduplicate and combine sources >>
-	CopyJob_PhaseThree: string+stat => [array of strings]
-
- */
