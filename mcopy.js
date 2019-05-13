d9119bcb39790cb5a07b8618ae03c8f77092f5a0
Alexander Kuznetsov <sasha@monterosa.co.uk>
2019-05-13 09:04:40 +0100

    Saving interim progress

--- mcopy.js
+++ mcopy.js
@@ -1,24 +1,40 @@
 'use strict';
 
+const Events = require('events');
+
 const parseArguments = require('./lib/parseArguments');
 const defaultOptions = require('./lib/defaultOptions');
-const InputCollection = require('./lib/InputCollection');
+const JobsCollection = require('./lib/ResolveSourcesCollection');
 
 module.exports = function (...args) {
-
+	// Parse and sanitise the inputs
 	let {jobs, opt, callback} = parseArguments(args);
-
 	opt = defaultOptions(opt);
-
-	let emitter = new Events();
-
-	let worker = new InputCollection(opt, emitter);
-
-	jobs.forEach((job) => worker.addJob(job));
-
-	let promise = worker.run();
-
-	emitter.promise = () => promise;
-
-	return emitter;
+	// This will eventually be returned
+	let api = new Events();
+	// Create a collection of copy jobs
+	let collection = new JobsCollection(jobs, opt, api);
+	// run() the collection automatically...
+	if (opt.autoStart) {
+		api.promise = collection.run();
+	}
+	// ...or add an API method to run() it manually
+	else {
+		// Intentionally use Deferred anti-pattern
+		let resolve, reject;
+		api.promise = new Promise((rs, rj) => {
+			resolve = rs;
+			reject = rj;
+		});
+		api.run = () => {
+			collection.run().then(resolve, reject);
+			return api.promise;
+		};
+	}
+	// Re-emit the promise state
+	api.promise.then((result) => api.emit('success', result), (error) => api.emit('error', error));
+	// Hook in a callback if it was provided
+	if (callback) api.promise.then((result) => callback(null, result), (err) => callback(err));
+	// Return created API object
+	return api;
 };
